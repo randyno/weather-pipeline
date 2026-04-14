d74deb830756e2c89cb5d266914f1508f70d33d8
randyno <randytchuiss@gmail.com>
2026-04-14 20:48:52 +0200

    Update data structure for visuals

--- dbt/weather_pipeline/models/aggregated_data/daily_aggregates.sql
+++ dbt/weather_pipeline/models/aggregated_data/daily_aggregates.sql
@@ -5,14 +5,14 @@ SELECT
     ROUND(AVG(temperature_2m), 2) AS avg_temperature_2m,
     ROUND(AVG(humidity), 2) AS avg_humidity,
     ROUND(AVG(apparent_temperature), 2) AS avg_apparent_temperature,
-    SUM(rain) AS total_rain,
+    ROUND(SUM(rain), 2) AS total_rain,
     ROUND(AVG(cloud_cover), 2) AS avg_cloud_cover,
     ROUND(AVG(wind_speed), 2) AS avg_wind_speed,
-    SUM(precipitation) AS total_precipitation,
-    MAX(temperature_2m) AS max_temperature_2m,
-    MIN(temperature_2m) AS min_temperature_2m,
-    MAX(temperature_2m) - MIN(temperature_2m) AS temperature_range,
-    MAX(apparent_temperature) - MIN(apparent_temperature) AS apparent_temperature_range,
+    ROUND(SUM(precipitation), 2) AS total_precipitation,
+    ROUND(MAX(temperature_2m),2) AS max_temperature_2m,
+    ROUND(MIN(temperature_2m),2) AS min_temperature_2m,
+    ROUND(MAX(temperature_2m) - MIN(temperature_2m),2) AS temperature_range,
+    ROUND(MAX(apparent_temperature) - MIN(apparent_temperature),2) AS apparent_temperature_range
 FROM {{ ref('cleaned_weather') }}
 GROUP BY day
 ORDER BY day
