232e24ec84f320ffcfa3f35f5071792257fd54a2
randyno <randytchuiss@gmail.com>
2026-04-25 00:50:40 +0200

    CI: Fix database tests

--- Tests/test_weather_dbt.py
+++ Tests/test_weather_dbt.py
@@ -5,15 +5,15 @@ from pathlib import Path
 class TestWeather_dbt:
 
     @pytest.mark.dependency(name="exists")
-    def test_database_exists(data_config):
-        database_file = data_config.data_relative_path + "weather_" + data_config.target + ".duckdb"
+    def test_database_exists(self, data_config):
+        database_file = data_config['data_relative_path'] + "weather_" + data_config['target'] + ".duckdb"
         
         # Make sure the database file exists and is not empty
         assert Path(database_file).exists(), f"Le fichier base de donnee {database_file} n'existe pas."
 
     @pytest.mark.dependency(depends = ['exists'])
-    def test_database_error(data_config):
-        database_file = data_config.data_relative_path + "weather_" + data_config.target + ".duckdb"
+    def test_database_error(self, data_config):
+        database_file = data_config['data_relative_path'] + "weather_" + data_config['target'] + ".duckdb"
         database_error_file = database_file + ".wal" 
         # This .duckdb.wal file stays if an error occurs when writing in the database file.
 
